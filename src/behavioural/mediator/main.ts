6f2830bb3c5e7080851e12c3e222ff0e1f181584
Talisson Avila <talisson_avila@hotmail.com>
2024-08-10 20:42:32 -0300

    add mediator gof

---
+++ src/behavioural/mediator/main.ts
@@ -0,0 +1,22 @@
+// import { Buyer } from './buyer';
+import { Mediator } from './mediator';
+import { Seller } from './seller';
+
+const mediator = new Mediator();
+const seller1 = new Seller();
+
+seller1.addProduct({ id: '1', name: 'mug', price: 10.22 });
+seller1.addProduct({ id: '2', name: 'pen', price: 1 });
+
+const seller2 = new Seller();
+seller2.addProduct({ id: '3', name: 'car', price: 15000.99 });
+seller2.addProduct({ id: '4', name: 'shirt', price: 12.46 });
+
+mediator.addSeller(seller1, seller2);
+
+// const buyer = new Buyer(mediator);
+seller2.viewProducts();
+seller2.buy('2');
+seller2.buy('3');
+
+seller2.viewProducts();
